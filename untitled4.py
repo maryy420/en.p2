f194693d6dde040584cf247dc15314ff8138724b
maryy420 <146622759+maryy420@users.noreply.github.com>
2023-10-14 23:10:29 +0330

    Update untitled4.py

--- untitled4.py
+++ untitled4.py
@@ -1,6 +1,6 @@
 def display_tuple():
     my_tuple = ("item1", "item2", "item3")
     print(my_tuple)
-    # del my_tuple[1]  #  با استفاده ازاندیس مکن نیست.( tuple)خیرحذف عنصردوم
+    # del my_tuple[1]  #  با استفاده ازاندیس ممکن نیست.( tuple)خیرحذف عنصردوم
 
 display_tuple()
